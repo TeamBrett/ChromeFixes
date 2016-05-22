bfbf4f9acc928268ea08878e92ddbaff59f61716
TeamBrett <teambrett9000@gmail.com>
2016-05-22 16:46:49 -0700

    add styles, icons, updates and stuff

--- github.ts
+++ github.ts
@@ -1,14 +1,14 @@
 // declare const DomReady;
 // DomReady.ready(function(){
-    if (window.location.href.match(/https:\/\/github.com\/(.*)\/pull\/(\d+)\/files/g)) {
-        var els = document.getElementsByClassName('repohead-details-container');
-        for(let i = 0; i < els.length; i++){
-            var el = els[i] as HTMLElement;
-            // if(el.style.width !== '980px'){
-            //     continue;
-            // }
-
-            el.style.width = '95%';
-        }
-    }
+//     if (window.location.href.match(/https:\/\/github.com\/(.*)\/pull\/(\d+)\/files/g)) {
+//         var els = document.getElementsByClassName('repohead-details-container');
+//         for(let i = 0; i < els.length; i++){
+//             var el = els[i] as HTMLElement;
+//             // if(el.style.width !== '980px'){
+//             //     continue;
+//             // }
+//
+//             el.style.width = '95%';
+//         }
+//     }
 // });
